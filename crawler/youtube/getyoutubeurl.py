70ecdc1d4a9e4e8479d2a4ebef7c478cf053e020
zhangxuanru <1107099011@qq.com>
2017-07-03 04:27:32 -0400

    Collating files

--- crawler/youtube/getyoutubeurl.py
+++ crawler/youtube/getyoutubeurl.py
@@ -1,4 +1,4 @@
- #!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Date    : 2016-02-28 20:17:55
 # @desc    : 获取youtube 地址分析出视频URL
@@ -15,7 +15,7 @@ import requests
 def find_sec():  
     url = "https://www.youtube.com/feed/trending"
     pa=re.compile(r'\w+')
-    browser = webdriver.Firefox() # Get local session of firefox  
+    browser = webdriver.PhantomJS() # Get local session of firefox  
     browser.get(url) # Load page  
     time.sleep(1)    # Let the page load  
     result=[]  
@@ -48,12 +48,12 @@ def find_sec():
                        
               board_url_len=len(board_title)
               for j in range(0,board_url_len):
-                video_title =  board_title[j].get_attribute('title').encode('utf-8')
+                video_title =  board_title[j].get_attribute('title')
                 video_href  =  board_title[j].get_attribute('href')
-              	if video_title =='' or video_href.find("watch") == -1:
+                if video_title =='' or video_href.find("watch") == -1:
               	   continue
-                requests.get(url='http://video.colsole.13520v.cc/crawler/youtube/savedata.php', params={'video_url':video_href,'video_title':video_title,'play_duration':video_time_txt,'video_time_content':update_time_txt,'video_cover':img_src}) 
-              	result.append([board_title[j].get_attribute('href'),board_title[j].get_attribute('title').encode('utf-8'),video_time_txt,update_time_txt,img_src])
+                requests.get(url='http://colsole.13520v.com/crawler/youtube/savedata.php', params={'video_url':video_href,'video_title':video_title,'play_duration':video_time_txt,'video_time_content':update_time_txt,'video_cover':img_src}) 
+                result.append([board_title[j].get_attribute('href'),board_title[j].get_attribute('title').encode('utf-8'),video_time_txt,update_time_txt,img_src])
 
         browser.close()  
         return result  
