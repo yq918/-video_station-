61777757b7f357a4ecac81dfba41f9b21daabeed
zhangxuanru <1107099011@qq.com>
2017-07-04 18:50:49 +0800

    add upload class

--- console/cmd/uploadqiniu.php
+++ console/cmd/uploadqiniu.php
@@ -5,7 +5,10 @@
  * @date   2017-07-01 23:12
  */
 include '../init.php';
+include '../config/conf.php';
+
 use models\db\db;
+use library\qiniu\Upload;
 
 class qiliu{
   public function run()
@@ -15,7 +18,10 @@ class qiliu{
   	if(empty($data)){
   		echo '没有视频可下载';
   		exit;
-  	}      
+  	}  
+
+    $Upload = new Upload();
+
          foreach ($data as $key => $value) {
   	       $video_img = $value['video_img'];
   	       if(!empty($video_img)){
@@ -29,13 +35,16 @@ class qiliu{
                  $pathArr = explode('/',$urlInfo['path']);
                  $file_name = array_pop($pathArr);
                  $file_path = $dir.'/'.$file_name;                  
-                 $call = "axel -n 2 -o  {$file_path}  {$video_img}";  
-
-                 echo $call;
-                 exit;
-
+                 $call = "axel -n 2 -o  {$file_path}  {$video_img}";                   
                  exec($call,$array); //执行命令
-	             usleep(1000);
+	               usleep(1000);
+                 if(file_exists($file_path)){
+                  //上传七牛
+                  $Upload -> upload_file($dirname,$file_path,$file_name);
+                   
+                 }
+               
+
 
             }
 
---
+++ console/library/qiniu/upload.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * 七牛上传文件类
+ */
+
+namespace library\qiniu;
+
+// 引入鉴权类
+use Qiniu\Auth;
+// 引入上传类
+use Qiniu\Storage\UploadManager;
+
+class Upload
+{
+	public function upload_file($bucket,$filePath,$key)
+	{
+
+		// 需要填写你的 Access Key 和 Secret Key
+		$accessKey = Access_Key;
+		$secretKey = Secret_Key;
+
+		// 构建鉴权对象
+		$auth = new Auth($accessKey, $secretKey);
+
+		// 要上传的空间
+		$bucket = 'images';
+
+		// 生成上传 Token
+		$token = $auth->uploadToken($bucket); 
+
+		// 初始化 UploadManager 对象并进行文件的上传。
+		$uploadMgr = new UploadManager();
+
+		// 调用 UploadManager 的 putFile 方法进行文件的上传。
+		list($ret, $err) = $uploadMgr->putFile($token, $key, $filePath);
+		echo "\n====> putFile result: \n";
+		if ($err !== null) {
+		    var_dump($err);
+		} else {
+		    var_dump($ret);
+		}
+
+   }
+
+}
