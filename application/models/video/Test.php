921db33919046ea9590c3f889c27ae14e4b4124a
zhangxuanru <1107099011@qq.com>
2017-07-11 19:43:19 +0800

    add rpc server

---
+++ application/models/video/Test.php
@@ -0,0 +1,17 @@
+<?php
+namespace Module;
+
+class Test
+{
+    static function test1($str = 'empty')
+    {
+        //var_dump(SOAServer::getClientEnv());
+        //var_dump(SOAServer::getRequestHeader());
+        return "hello-soa-finish: $str";
+    }
+
+    static function hello()
+    {
+        return array('key1' => 'A', 'key2' => 'B');
+    }
+}
