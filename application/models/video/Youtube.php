a4d8fa3e89d68f740db78d13d662f50795039199
zhangxuanru <1107099011@qq.com>
2017-07-12 15:36:47 +0800

    update return data

--- application/models/video/Youtube.php
+++ application/models/video/Youtube.php
@@ -27,18 +27,17 @@ class Youtube
         $fields = array('where'=>"video_down=1 and video_img_down=1 AND status=1 AND cat_id={$cat_id} ", 'limit'=>"{$start},{$limit}", 'order'=>$order, 'field'=>'id','table'=> self::YOUTUBETABLE);
         $data =  $this->db->fList($fields);
         if(empty($data)){
-            return $data;
+              return array('data' => $data);
         }
         $you_id_list = array_column($data,'id');
         $you_id_str  = implode(',',$you_id_list);
-        $fields = array('where'=>"img_upload=1 and video_upload=1 AND status=1 AND you_id in ({$you_id_str}) ",
-                        'limit'=>"{$start},{$limit}",
-                        'field'=>'id,video_title,play_duration,image_file_name,video_file_name,video_upload,video_time_content',
-                        'table'=> self::YOUTUBEDOWNTABLE);
-
+        $fields = array('where' => "img_upload=1 and video_upload=1 AND status=1 AND you_id in ({$you_id_str}) ",
+                        'limit' => "{$start},{$limit}",
+                        'order' => $order,
+                        'field' => 'id,video_title,play_duration,image_file_name,video_file_name,video_upload,video_time_content',
+                        'table' => self::YOUTUBEDOWNTABLE);
         $you_data = $this->db->fList($fields);
-
-        return array( 'data' => $data,'you_data' => $you_data);
+        return array('data' => $data, 'you_data' => $you_data);
     }
 
 }
